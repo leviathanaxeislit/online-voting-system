c9579085722d326eec9c0346e7161ff6c7a32018
leviathanaxeislit <scp.thedevil@gmail.com>
2024-06-02 20:47:12 +0530

    Helmet
    Signed-off-by: leviathanaxeislit <scp.thedevil@gmail.com>

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
-const userRouter = require('./routers/usersroute')
+const helmet = require('helmet');
+const userRouter = require('./routers/usersroute');
 
-const port = process.env.PORT
-const app = express()
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(helmet()); // Security middleware
 app.use(cookieParser());
-app.use(express.json())
-//only for development
+app.use(express.json());
+
+// CORS Configuration
 app.use(cors({
-    origin: ['http://localhost:3000', "https://onlinevotingsystemclient.vercel.app/"],  // <-- location of the react app were connecting to
+    origin: ['http://localhost:3000', 'https://onlinevotingsystemclient.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-
 }));
-// app.use(cors());
 
-const db = require("./db")
+const db = require('./db');
+
 
 app.get("/", (req, res) => {
-	res.send("Hello World!")
-})
+    res.send("Hello World!");
+});
 
-app.use('/api/user',userRouter)
+app.use('/api/user', userRouter);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-})
+});
